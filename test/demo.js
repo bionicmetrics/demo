f3ae88b0e13af247172001b9d60e8ec30d42c2c5
Bionic Metrics <demo@bionicmetrics.com>
2020-03-09 18:01:07 +0000

    nayteDogg Changes

--- test/demo.js
+++ test/demo.js
@@ -6,7 +6,8 @@
 // axios - for dem api calls and such
 // macro - macros for doing things like getting elements and other neat things
 async function test(baseUrl, driver, data) {
-    logger.info(`Demo test ${new Date()}`);
+    await logger.info(`Demo test ${new Date()}`);
+    await logger.info(`Building out this test the naytedogg is doing, woop woop!`);
 }
 
 test(baseUrl, driver, data); // implicitly returns a promise
