b296b4976e56b3517f86b7998180b86b2f1c1d71
Bionic Metrics <demo@bionicmetrics.com>
2020-03-10 19:58:20 +0000

    This is a commit from nayteDoggs

---
+++ test/myTest.js
@@ -0,0 +1,12 @@
+// Available:
+// console - ya know, for simple logging (doesn't go anywhere)
+// logger - logger that will stream
+// expect - Jest's expect
+// {Key, By, until} - selenium webdriver
+// axios - for dem api calls and such
+// macro - macros for doing things like getting elements and other neat things
+async function test(baseUrl, driver, data) {
+    await logger.info(`This is a test in NayteDoggs Branch`);
+}
+
+test(baseUrl, driver, data); // implicitly returns a promise
