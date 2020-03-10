64f2b1f69da833d41304152a8da3173cb8c320e9
Bionic Metrics <demo@bionicmetrics.com>
2020-03-10 20:02:49 +0000

    commit 2

--- test/myTest.js
+++ test/myTest.js
@@ -7,6 +7,7 @@
 // macro - macros for doing things like getting elements and other neat things
 async function test(baseUrl, driver, data) {
     await logger.info(`This is a test in NayteDoggs Branch`);
+    await logger.info(`This is my next change`);
 }
 
 test(baseUrl, driver, data); // implicitly returns a promise
